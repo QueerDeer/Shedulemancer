d0f08de5e92a00730fb54fc93e19f41d5d562b56
Aleksander Belozyorov <coyqueerdeer@gmail.com>
2018-01-30 08:39:35 +0300

    try fix regular reminder, other small hot fixes

--- main_handler.py
+++ main_handler.py
@@ -4,6 +4,8 @@
 import requests
 import datetime
 
+# also we are in need of class for processing methods (reminder of schedule, answerer...)
+
 
 class BotHandler:
     def __init__(self, token):
@@ -13,14 +15,18 @@ class BotHandler:
     def get_updates(self, offset=None, timeout=5):
         method = 'getUpdates'
         params = {'timeout': timeout, 'offset': offset}
+
         resp = requests.get(self.api_url + method, params)
         result_json = resp.json()['result']
+
         return result_json
 
     def send_message(self, chat_id, text):
         params = {'chat_id': chat_id, 'text': text}
         method = 'sendMessage'
+
         resp = requests.post(self.api_url + method, params)
+
         return resp
 
     def get_last_update(self):
@@ -33,37 +39,47 @@ class BotHandler:
 
         return last_update
 
+# buildup, missing pairs of tasks for list of scheds
 
 token = "545213183:AAF2vAqvhV_YTgP-LUZrV3vsBkF6iNbNWJA"
+test_chat_id = -1001192271209  # 331's chat_id
+
 greet_bot = BotHandler(token)
+
 greetings = '/knockhead'
+scheds = ('/mon', '/tue', '/wed', '/thu', '/fri', '/sat')
+
 now = datetime.datetime.now()
 
 
 def main():
     new_offset = None
     today = now.day
+    next_day = now.day
     hour = now.hour
-    minute = now.minute
 
     while True:
         greet_bot.get_updates(new_offset)
 
         last_update = greet_bot.get_last_update()
+
+        if today == next_day and hour == 6:  # our time is +3 hours
+            greet_bot.send_message(test_chat_id, 'Today is {}'.format(today.strftime("%A")))
+            next_date = now + datetime.timedelta(days=1)
+            next_day = next_date.day
+
         if not last_update:
             continue
+
         last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
+        last_chat_text = last_update['message']['text']  # sometimes it works wrong
         last_chat_id = last_update['message']['chat']['id']
         last_chat_name = last_update['message']['from']['first_name']
 
         if last_chat_text.lower() in greetings:
             greet_bot.send_message(last_chat_id, 'NO U, {}'.format(last_chat_name))
 
-        if today == now.day and hour == 4 and minute == 20:
-            greet_bot.send_message(last_chat_id, 'Today is {}'.format(today.strftime("%A")))
-
-        new_offset = last_update_id + 1
+        new_offset = last_update_id + 1  # mmm... it works?
 
 
 if __name__ == '__main__':
@@ -71,3 +87,4 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         exit()
+
